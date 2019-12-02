ec58890147e8b65bc640c3bb63777f897c9359a4
Wilcarl Lopez <wilcarllopez@gmail.com>
2019-12-02 20:10:27 +0800

    Updated and added new files

--- harvest_app/db_manage.py
+++ harvest_app/db_manage.py
@@ -1,93 +1,516 @@
-import configparser
-import getpass
-import logging
-import os
 import psycopg2
 
-logger = logging.getLogger(__name__)
+import logging
+
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-def connect_to_db(username, hostname, password, database):
-    """
-    Connect to a database.
-    :param username: DB username.
-    :param hostname: DB hostname.
-    :param password: DB password.
-    :param database: DB name.
-    :return: Connection object.
-    """
-    logger.info(f"Connecting to {database} database")
+
+
+
+def create_db(user, password, host, dbname):
+
     try:
-        logger.info(f"Connected to {database} database")
-        connection = psycopg2.connect(user=username,
+
+        connection = psycopg2.connect(user=user,
+
                                       password=password,
-                                      host=hostname,
-                                      port="5432",
-                                      database=database)
+
+                                      host=host,
+
+                                      port="54320",
+
+                                      database="postgres")
+
         connection.autocommit = True
-        return connection
-    except (Exception, psycopg2.Error) as error:
-        logger.error(f"Error while connecting to PostgreSQL: {error}")
+
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        cursor = connection.cursor()
+
+        find_db_query = "SELECT datname FROM pg_catalog.pg_database WHERE datname = '{}';".format(dbname)
+
+        cursor.execute(find_db_query)
+
+        if cursor.fetchone():
+
+            pass
+
+        else:
+
+            create_db_query = "CREATE DATABASE {};".format(dbname)
 
 
-def create_db(db_name, username, hostname):
-    """
-    Check for and create a database.
-    :param db_name: DB name.
-    :param username: DB username.
-    :param hostname: DB hostname.
-    :return: Connection object.
-    """
-    password = getpass.getpass("Password: ")
-    connection = connect_to_db(username, hostname, password, "postgres")
-    cursor = connection.cursor()
 
-    logger.info("Checking for existing database")
-    cursor.execute(f"SELECT exists(SELECT 1 from pg_catalog.pg_database WHERE datname = '{db_name}');")
-    if cursor.fetchone():
-        logger.info("Database exists")
-    else:
-        logger.info("Database does not exist, creating new one")
-        cursor.execute(f"CREATE DATABASE {db_name};")
-    cursor.close()
-    connection.close()
+            cursor.execute(create_db_query)
 
-    return connect_to_db(username, hostname, password, db_name)
+            connection.commit()
+
+        # print("Table created successfully in PostgreSQL ")
 
 
-def write_to_db(row, db_name, username, hostname):
-    """
-    Insert list of file to database.
-    :param file_list: List of files.
-    :param username: DB username.
-    :param hostname: DB hostname.
-    :return: None
-    """
-    config = configparser.ConfigParser()
-    path = os.path.abspath(os.path.dirname(__file__))
-    config.read(f"{path}{os.sep}config.ini")
-    connection = create_db(db_name, username, hostname)
-    cursor = connection.cursor()
-    connection.commit()
-    logger.info("Checking for existing table")
-    query = f"SELECT to_regclass('{config['database']['table_name']}');"
-    cursor.execute(query)
-    if cursor.fetchone()[0]:
-        logger.info("Table found")
 
-    else:
-        logger.info("Table missing, creating new one")
-        tb_name = config['database']['table_name']
-        create_table_query = f"CREATE TABLE IF NOT EXISTS {tb_name} (ID SERIAL PRIMARY KEY, " \
-                             f"APP_NAME TEXT NOT NULL, APP_VERSION TEXT NOT NULL);"
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error while creating PostgreSQL table", error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            print("PostgreSQL connection is closed")
+
+
+
+
+
+def establish_connection(user, password, host, dbname):
+
+    create_db(user, password, host, dbname)
+
+    connection = psycopg2.connect(user=user,
+
+                                  password=password,
+
+                                  host=host,
+
+                                  port="54320",
+
+                                  database=dbname)
+
+    return connection
+
+
+
+
+
+def create_table_details(user, password, host, dbname):
+
+    try:
+
+        connection = establish_connection(user, password, host, dbname)
+
+        cursor = connection.cursor()
+
+
+
+        create_table_query = '''CREATE TABLE IF NOT EXISTS downloadpages
+
+              (ID SERIAL PRIMARY KEY     NOT NULL,
+
+              Name           VARCHAR    NOT NULL,
+
+              Version        VARCHAR); '''
+
+
+
         cursor.execute(create_table_query)
-        logger.info(f"Table {tb_name} created")
 
-    logger.info(f"Writing to {db_name} database")
-    query = '''INSERT INTO output
-            (APP_NAME, APP_VERSION) 
-            VALUES (%s, %s)'''
+        connection.commit()
 
-    cursor.execute(query, row)
-    cursor.close()
-    connection.close()
+        # print("Table created successfully in PostgreSQL ")
+
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error while creating PostgreSQL table", error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # print("PostgreSQL connection is closed")
+
+
+
+
+
+def create_table_links(user, password, host, dbname):
+
+    try:
+
+        connection = establish_connection(user, password, host, dbname)
+
+
+
+        cursor = connection.cursor()
+
+
+
+        create_table_query = '''CREATE TABLE IF NOT EXISTS downloadlinks
+
+              (ID SERIAL PRIMARY KEY     NOT NULL,
+
+              Link         VARCHAR    NOT NULL); '''
+
+
+
+        cursor.execute(create_table_query)
+
+        connection.commit()
+
+        # print("Table created successfully in PostgreSQL ")
+
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error while creating PostgreSQL table", error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # print("PostgreSQL connection is closed")
+
+
+
+
+
+def create_table_translations(user, password, host, dbname):
+
+    try:
+
+        connection = establish_connection(user, password, host, dbname)
+
+
+
+        cursor = connection.cursor()
+
+
+
+        create_table_query = '''CREATE TABLE IF NOT EXISTS downloadtranslations
+
+              (ID SERIAL PRIMARY KEY     NOT NULL,
+
+              Link         VARCHAR    NOT NULL,
+
+              Version      VARCHAR    NOT NULL); '''
+
+
+
+        cursor.execute(create_table_query)
+
+        connection.commit()
+
+        # print("Table created successfully in PostgreSQL ")
+
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error while creating PostgreSQL table", error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # print("PostgreSQL connection is closed")
+
+
+
+
+
+#  CREATE
+
+def insert_links(user, password, host, dbname, link):
+
+    try:
+
+        create_table_links(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+
+
+        # connection.autocommit = True
+
+        # connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+
+
+        cursor = connection.cursor()
+
+        insert_query = "INSERT INTO downloadlinks (Link)" \
+
+                       " VALUES(%s)"
+
+        cursor.execute(insert_query, (link,))
+
+        print(link)
+
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+
+        # logger.error('Error while connecting to PostgreSQL: ' + str(error), exc_info=True)
+
+        print(error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # logger.info("PostgreSQL connection is closed")
+
+
+
+
+
+def insert_details(user, password, host, dbname, dl_dict):
+
+    try:
+
+        create_table_details(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+
+
+        connection.autocommit = True
+
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+
+
+        cursor = connection.cursor()
+
+        insert_query = "INSERT INTO downloadpages(Name, Version)" \
+
+                       " VALUES(%(name)s,%(version)s)"
+
+        cursor.execute(insert_query, dl_dict)
+
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+
+        # logger.error('Error while connecting to PostgreSQL: ' + str(error), exc_info=True)
+
+        print(error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # logger.info("PostgreSQL connection is closed")
+
+
+
+
+
+def insert_translations(user, password, host, dbname, translation_dict):
+
+    try:
+
+        create_table_details(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+
+
+        connection.autocommit = True
+
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+
+
+        cursor = connection.cursor()
+
+        insert_query = "INSERT INTO downloadtranslations(Link, Version)" \
+
+                       " VALUES(%(language)s,%(version)s)"
+
+        cursor.execute(insert_query, translation_dict)
+
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+
+        # logger.error('Error while connecting to PostgreSQL: ' + str(error), exc_info=True)
+
+        print(error)
+
+    finally:
+
+        # closing database connection.
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+            # logger.info("PostgreSQL connection is closed")
+
+
+
+
+
+#  READ
+
+def select_links(user, password, host, dbname, link):  # check if the link is already in the table 'downloadlinks'
+
+    try:
+
+        create_table_links(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+        cursor = connection.cursor()
+
+
+
+        search_query = "SELECT * FROM downloadlinks WHERE Link = '{}'".format(link)
+
+        cursor.execute(search_query)
+
+        result = cursor.fetchone()
+
+        if result is None:
+
+            return True
+
+        else:
+
+            return False
+
+    except (Exception, psycopg2.Error) as error:
+
+        print(error)
+
+    # finally:
+
+    #     if (connection):
+
+    #         cursor.close()
+
+    #         connection.close()
+
+
+
+
+
+def select_details(user, password, host, dbname, detail_dict):
+
+    try:
+
+        create_table_details(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+        cursor = connection.cursor()
+
+
+
+        search_query = "SELECT * FROM downloadpages WHERE Name='{}' and Version ='{}'".format(detail_dict["name"],
+
+                                                                                              detail_dict["version"])
+
+        cursor.execute(search_query)
+
+        result = cursor.fetchone()
+
+        if result is None:
+
+            return True
+
+        else:
+
+            return False
+
+    except (Exception, psycopg2.Error) as error:
+
+        print(error)
+
+    finally:
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
+
+
+
+
+def select_translations(user, password, host, dbname, translation_dict):
+
+    try:
+
+        create_table_translations(user, password, host, dbname)
+
+        connection = establish_connection(user, password, host, dbname)
+
+        cursor = connection.cursor()
+
+
+
+        search_query = "SELECT * " \
+
+                       "FROM downloadtranslations " \
+
+                       "WHERE Link='{}' and Version ='{}'".format(translation_dict["language"], translation_dict["version"])
+
+        cursor.execute(search_query)
+
+        result = cursor.fetchone()
+
+        if result is None:
+
+            return True
+
+        else:
+
+            return False
+
+    except (Exception, psycopg2.Error) as error:
+
+        print(error)
+
+    finally:
+
+        if (connection):
+
+            cursor.close()
+
+            connection.close()
+
--- harvest_app/harvest.py
+++ harvest_app/harvest.py
@@ -1,208 +1,194 @@
-import configparser
-import db_manage
-import logging
-import logging.config
-import os
-import re
 import requests
-import sys as sys
-import yaml
-# absolute imports
+
+from requests import get
+
+import re
+
+import configparser
+
+import os
+
 from bs4 import BeautifulSoup
 
+from urllib.parse import urljoin
 
-def link_details(url):
-    """
-    Get the application's name and version
-    :param url: Download link
-    :return: app_name, version
-    """
-    soup = request_get(url)
-    details = soup.find(text=re.compile("v[0-9]*\.[0-9]*"))
-    logger.info(f"Getting download link details from {url}")
-    if details is None:
-        row = ("", "")
-        pass
-    else:
-        index = re.search("v[0-9]*\.[0-9]*", details.split("-")[0])
-        app_name = details.split("-")[0][0:index.start()].replace("\n", "")
-        app_version = details.split("-")[0][index.start():].replace("\n", "")
-        row = [app_name, app_version]
-    find_translations(url)
-    # for link in row:
-    # save_to_db(link)
+import db_manage as db
 
-    logger.info(row)
+config = configparser.ConfigParser()
+
+conf_dir = os.path.join(os.path.dirname(__file__), 'conf.ini')
+
+config.read(conf_dir)
+
+password = config['args']['password']
+
+hostname = config['args']['hostname']
+
+username = config['args']['username']
+
+dbname = config['args']['dbname']
 
 
-def find_translations(link):
-    soup = request_get(link)
-    try:
-        identifier = soup.find_all("tr", class_="utiltableheader")[-1]
-        table = identifier.find_parent("table")
-        row = table.find_all("tr")[1:]  # [1:] to disregard the table header
-        translations = []
-        for item in row:
-            language = item.find_all("td")[0].find("a").get("href").rsplit()
-            version = item.find_all("td")[-1].text.replace("\n", "").rsplit()
-            translation_details = {"language": urljoin(link, language), "version": version}
-            logger.info(translation_details)
-            check_translations(translation_details)
-            translations.append(translation_details)
-    except:
-        translations = None
-    return translations
+def parse_link(link):
+    url = link
 
+    resource = requests.get(url)
 
-def download_files(url, base_url):
-    """
-    :param url: Download link
-    :return url_list: List of download links
-    """
-    soup = request_get(url)
-    suffix = ["zip", "exe"]
-    url_list = []
-    for link in soup.find_all("a", href=True):
-        if "http" not in link.get('href'):
-            for suff in suffix:
-                if suff in link.get('href'):
-                    url_list.append(link.get('href'))
-    # url_list = [a['href'] for suff in suffix for a in soup.find_all('a', href=True)
-    #             if "http" or "html" not in a.get('href') if suff in a.get('href')]
-    # print(url_list)
-    get_download_files(url_list, base_url)
-    for link in url_list:
-        find_translations(link)
+    soup = BeautifulSoup(resource.text, 'lxml')
 
-    return url_list
-
-
-def get_download_files(url_list, base_url):
-    path = config['download']['path']
-    for link in url_list:
-        if "trans" in link:
-            r = requests.get(f"{base_url}/utils/{link}")
-            logger.info(f"Downloading file from: {base_url}/utils/{link}")
-            filename = link.split("/")[-1]
-            directory = f"{os.path.abspath(os.path.dirname(__name__))}{os.sep}{path}{os.sep}{filename}"
-            if os.path.exists(directory):
-                logger.info("File already exists")
-                pass
-            else:
-                with open(directory, 'wb') as f:
-                    f.write(r.content)
-        else:
-            link = link.replace("..", "")
-            r = requests.get(f"{base_url}{link}")
-            logger.info(f"Downloading file from: {base_url}{link}")
-            filename = link.split("/")[-1]
-            directory = f"{os.path.abspath(os.path.dirname(__name__))}{os.sep}{path}{os.sep}{filename}"
-            if os.path.exists(directory):
-                logger.info("File already exists")
-                pass
-            else:
-                with open(directory, 'wb') as f:
-                    f.write(r.content)
-
-
-def save_to_db(row):
-    db_manage.write_to_db(str(row), config['database']['db_name'], config['database']['username'],
-                          config['database']['hostname'])
-
-
-def request_get(url):
-    """
-    Request and get the url
-    :param url: URL provided
-    :return soup: parsed text
-    """
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-        logger.info(f"Successfully established a new connection to {url}")
-        soup = BeautifulSoup(r.text, 'lxml')
-    except requests.exceptions.ConnectionError:
-        logger.error(f"Failed to establish a new connection to {url}")
-        sys.exit(1)
     return soup
 
 
-def setup_logging(default_path='logging_config.yml', default_level=logging.INFO, env_key='LOG_CFG'):
-    """Setting up the logging config"""
-    path = default_path
-    value = os.getenv(env_key, None)
-    if value:
-        path = value
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            try:
-                config = yaml.safe_load(f.read())
-                logging.config.dictConfig(config)
-            except Exception as e:
-                print('Error in Logging Configuration. Using default configs', e)
-                logging.basicConfig(level=default_level, stream=sys.stdout)
+def download_file(url, filename):
+    with open("{}{}{}{}{}".format(os.path.abspath(os.path.dirname(__name__)), os.sep, "downloads", os.sep, filename),
+              "wb") as file:
+        response = get(url)
+
+        file.write(response.content)
+
+
+def get_all_downloadable(download_page_link):
+    soup = parse_link(download_page_link)
+
+    name_version = get_details(download_page_link)
+
+    db.insert_details(username, password, hostname, dbname, name_version)
+
+    download_links = soup.find_all("a", {"class": "downloadline"})
+
+    # download_links = soup.find_all("a", href=True)
+
+    acceptable_ext = ["exe", "zip"]
+
+    downloadable_files = []
+
+    for file in download_links:
+
+        file_link = file.get("href")
+
+        if file_link.split(".")[-1] in acceptable_ext:
+            download_link = urljoin(download_page_link, file_link)
+
+            downloadable_files.append(download_link)
+
+            download_file(download_link, download_link.rsplit("/", 1)[-1])
+
+            print(download_link)
+
+    return downloadable_files
+
+
+def check_version(download_page_link):
+    name_version = get_details(download_page_link)
+
+    if db.select_details(username, password, hostname, dbname, name_version):
+
+        get_all_downloadable(download_page_link)
+
+        return True
+
     else:
-        logging.basicConfig(level=default_level, stream=sys.stdout)
-        print('Failed to load configuration file. Using default configs')
+
+        return False
 
 
-def write_to_file(fullpath):
-    """
-    Write a list of download links to a text file
-    :param fullpath: Downloadable links
-    :return:
-    """
-    path = config['default']['path']
-    filename = config['default']['filename']
-    save_path = f"{path}{os.sep}{filename}"
-    with open(save_path, 'a+') as writer:
-        writer.write(fullpath + "\n")
+def get_details(download_page_link):
+    soup = parse_link(download_page_link)
+
+    version_regex = "v[0-9]*\.[0-9]*"
+
+    details = soup.find(text=re.compile(version_regex))
+
+    name_version = details.split("-")[0]
+
+    index = re.search(version_regex, name_version)
+
+    name = name_version[0:index.start()].replace("\n", "")
+
+    version = name_version[index.start():].replace("\n", "")
+
+    return {"name": name, "version": version}
 
 
-def main(base_url):
-    """
-    Main function of the program: collects downloadable links
-    :param base_url: Mirror link of Nirsoft.net
-    :return: List of downloadable links
-    """
-    soup = request_get(base_url)
-    counter = 0
-    download_links = []
-    auxiliary_list = []
-    url_list = soup.find('ul')
+def check_translations(translations):
+    if db.select_translations(username, password, hostname, dbname, translations):
+        download_link = translations['language']
 
-    """Checks for download links"""
-    for url in url_list.find_all('a', href=True, recursive=True):
-        if "nirsoft" not in str(url['href']):
-            if "http" not in str(url['href']):
-                fullpath = str(base_url + url['href'])
-            else:
-                fullpath = url['href']
-            logger.info(f"Found the link: {fullpath}")
-            download_links.append(fullpath)  # appends the url to a list
+        version = translations['version']
 
-    """Getting the unique value of download links"""
-    for link in download_links:
-        if link not in auxiliary_list:
-            auxiliary_list.append(link)
-            counter += 1
-            logger.info(f"{counter} - {link} appended to the final list")
+        download_file(download_link, version + download_link.rsplit("/", 1)[-1])
 
-    for link in auxiliary_list:
-        link_details(link)
-        # write_to_file(link)
-        download_files(link, base_url)
+        db.insert_translations(username, password, hostname, dbname, translations)
 
-        # write_to_db()
 
-    return auxiliary_list
+def get_translations(download_page_link):
+    different_link = ["http://54.174.36.110/utils/internet_explorer_cookies_view.html"]
+
+    if download_page_link not in different_link:
+
+        soup = parse_link(download_page_link)
+
+        identifier = soup.find_all("tr", class_="utiltableheader")[-1]
+
+        table = identifier.find_parent("table")
+
+        row = table.find_all("tr")[1:]  # [1:] to disregard the table header
+
+        translations = []
+
+        for item in row:
+            language = item.find_all("td")[0].find("a").get("href")
+
+            version = item.find_all("td")[-1].text.replace("\n", "")
+
+            translation_details = {"language": urljoin(download_page_link, language), "version": version}
+
+            check_translations(translation_details)
+
+            translations.append(translation_details)
+
+        return translations
+
+
+def get_links():
+    base_url = "http://54.174.36.110/"
+
+    soup = parse_link(base_url)
+
+    unordered_list = soup.find("ul")
+
+    index_links = unordered_list.find_all("a", href=True)
+
+    download_pages = []
+
+    for link in index_links:
+
+        href = link.get("href")
+
+        if "http" not in href and db.select_links(username, password, hostname, dbname, href):
+            download_page_link = urljoin(base_url, href)  # url of the download page
+
+            download_pages.append(download_page_link)  # get details of download pages then save to list
+
+            # check_version(download_page_link)  # checker of version changes
+
+            db.insert_links(username, password, hostname, dbname, href)  # checker of link duplicates
+
+    for pages in download_pages:
+        check_version(pages)
+
+        get_translations(pages)
+
+    return download_pages
+
+
+def main():
+    get_links()
+
+    # download_file("http://54.174.36.110/utils/trans/webbrowserpassview_arabic.zip" ,"trail.zip")
+
+    # get_translations("http://54.174.36.110/utils/iehv.html")
 
 
 if __name__ == '__main__':
-    setup_logging()
-    logger = logging.getLogger(__name__)
-    config = configparser.ConfigParser()
-    path = os.path.abspath(os.path.dirname(__file__))
-    config.read(f"{path}{os.sep}config.ini")
-    base_url = config['default']['harvest_url']
-    main(base_url)
+    main()
